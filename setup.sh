2713501dcc7224f148c8da2431d0e71a635e3e71
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-01-20 10:12:59 -0300

    local: ajustando arquivos para subida da aplicação local

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Access local folder
+cd local || exit
+
 # Stop and remove containers, networks, images, and volumes
 docker-compose down -v
 
+# Return to project folder
+cd .. || exit
+
 # Clean and build the Maven project
 ./mvnw clean install
+
+# Access local folder
+cd local || exit
 
 # Start the containers in detached mode and build the images
 docker-compose up -d --build
